1d6668d9c52ee7bcf4868ef1ff12210079dcb234
Prashanth Sunku <123313516+prashanthsunku9@users.noreply.github.com>
2023-02-24 23:59:16 +0530

    Add files via upload

---
+++ MILESTONE-1/CONDITIONALS AND LOOPS/sum.cpp
@@ -0,0 +1,56 @@
+<<<<<<< HEAD
+/*Sum of Even Numbers till N
+
+Given a number N, print sum of all even numbers from 1 to N
+
+Input Format :Integer N
+Output Format :Required Sum 
+
+
+Sample Input 1 :
+ 6
+Sample Output 1 :
+12*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;cin>>n;
+
+    int sum=0;
+    for(int i=0;i<=n;i++){
+        if(i%2==0){
+            sum+=i;
+        }
+    }
+    cout<<sum<<endl;
+=======
+/*Sum of Even Numbers till N
+
+Given a number N, print sum of all even numbers from 1 to N
+
+Input Format :Integer N
+Output Format :Required Sum 
+
+
+Sample Input 1 :
+ 6
+Sample Output 1 :
+12*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;cin>>n;
+
+    int sum=0;
+    for(int i=0;i<=n;i++){
+        if(i%2==0){
+            sum+=i;
+        }
+    }
+    cout<<sum<<endl;
+>>>>>>> master
+}
